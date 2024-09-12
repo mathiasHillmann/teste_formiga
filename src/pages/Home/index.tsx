11a85e5425542d736566d91586c30a2be567c5e7
Mathias Oliveira <mathias.oliveira@nexello.com.br>
2024-09-12 09:24:44 -0300

    home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export const Home: React.FC = () => {
-  return <p>Teste</p>;
+  return (
+    <p>
+      Esta aplicação serve para testar APIs que encontro por ai, assim como também testar o{' '}
+      <a href="https://ant.design/">Ant Design</a> com o <a href="https://react.dev/">React</a>
+    </p>
+  );
 };
